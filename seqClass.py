f0cda38212d064422772b0a441a089eaa617e0eb
7avista <7abustia@gmail.com>
2021-04-05 14:44:08 +0200

    modified file to push

--- seqClass.py
+++ seqClass.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+d#!/usr/bin/env python
 
 import sys, re                             #  add sys var and access to re module (RegEx)
 from argparse import ArgumentParser
@@ -20,6 +20,20 @@ if re.search('^[ACGTU]+$', args.seq):       # defines the re expression in var
         print ('The sequence is inconsistent')
     elif re.search('T', args.seq):            #  looks for T in var to diff to DNA
         print ('The sequence is DNA')
+	for i in  len(args.seq):
+		t = 0
+		c = 0
+		g = 0
+		a = 0
+		if re.search('T'):
+			t =  1 + t
+		elif re.search('C'):
+                        c =  1 + c
+		elif re.search('G'):
+                        g =  1 + g
+		else:
+			a = 1 + a 
+	print ('The total content of each base is: '\n + 'T' + t/len(args.seq)+100 + '%') 
     elif re.search('U', args.seq):          # looks for U in var to diff to RNA
         print ('The sequence is RNA')
     else:                                   # rest of cases unable to diff
